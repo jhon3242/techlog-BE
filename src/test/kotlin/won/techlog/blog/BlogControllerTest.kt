2cd75e19d299e604c0e14b8b86ca2356f91e07b3
jhon3242 <rldotnr2@gmail.com>
2025-04-22 11:07:03 +0700

    test: 테스트 엔드포인트 변경

--- src/test/kotlin/won/techlog/blog/BlogControllerTest.kt
+++ src/test/kotlin/won/techlog/blog/BlogControllerTest.kt
@@ -3,6 +3,10 @@ package won.techlog.blog
 import io.restassured.RestAssured
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import won.techlog.blog.api.request.BlogRequest
+import won.techlog.blog.api.response.BlogResponse
 import won.techlog.poster.api.response.PosterResponse
 import won.techlog.support.BaseControllerTest
 
@@ -11,9 +15,11 @@ class BlogControllerTest: BaseControllerTest() {
     @Test
     fun `우아한형제들 블로그 글을 찾는다`() {
         // given
+        val request = BlogRequest("https://techblog.woowahan.com/?paged=1")
         val result = RestAssured.given().log().all()
-            .param("url", "https://techblog.woowahan.com/?paged=1")
-            .`when`().get("/api/blog/posters")
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .body(request)
+            .`when`().post("/api/blogs")
             .then().log().all()
             .extract()
 
@@ -26,17 +32,19 @@ class BlogControllerTest: BaseControllerTest() {
     @Test
     fun `우아한형제들 블로그 글을 파싱한다`() {
         // given
+        val request = BlogRequest("https://techblog.woowahan.com/21604/")
         val response = RestAssured.given().log().all()
-            .param("url", "https://techblog.woowahan.com/21604/")
-            .`when`().get("/api/blog/poster")
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .body(request)
+            .`when`().post("/api/blog")
             .then().log().all()
-            .extract().`as`(PosterResponse::class.java)
+            .extract().`as`(BlogResponse::class.java)
 
         // when & then
         Assertions.assertThat(response.url).isNotEmpty()
         Assertions.assertThat(response.title).isNotEmpty()
         Assertions.assertThat(response.thumbnail).isNotEmpty()
         Assertions.assertThat(response.content).isNotEmpty()
+        Assertions.assertThat(response.blogType).isNotEmpty()
     }
-
 }
