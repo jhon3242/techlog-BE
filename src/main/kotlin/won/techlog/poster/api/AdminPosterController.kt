3bd91d266023cfd834d8501bfc977257733c6bf1
jhon3242 <rldotnr2@gmail.com>
2025-04-26 18:34:45 +0700

    test: 파싱 테스트 비활성화

---
+++ src/main/kotlin/won/techlog/poster/api/AdminPosterController.kt
@@ -0,0 +1,41 @@
+package won.techlog.poster.api
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import won.techlog.common.admin.AdminCheck
+import won.techlog.poster.api.request.PosterCreateRequest
+import won.techlog.poster.api.request.PostersCreateRequest
+import won.techlog.poster.api.response.PosterResponse
+import won.techlog.poster.api.response.PostersResponse
+import won.techlog.poster.domain.PosterService
+
+@AdminCheck
+@RestController
+@RequestMapping("/api")
+class AdminPosterController(
+    private val posterService: PosterService
+) {
+    @PostMapping("/poster")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createPoster(@RequestBody request: PosterCreateRequest): PosterResponse {
+        val poster = posterService.createPoster(request.toPoster())
+        return PosterResponse(poster)
+    }
+
+    @PostMapping("/posters")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createPosters(@RequestBody requests: PostersCreateRequest): PostersResponse {
+        val posters = posterService.createPosters(requests.toPosters())
+        return PostersResponse(posters.map { PosterResponse(it) })
+    }
+
+    @DeleteMapping("posters/{id}")
+    fun deletePoster(@PathVariable id: Long)
+            = posterService.deletePoster(id)
+}
