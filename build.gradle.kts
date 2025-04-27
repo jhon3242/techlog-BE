a99298fe0b2bd84784cd7b697e1efe8c4b7d0c73
jhon3242 <rldotnr2@gmail.com>
2025-04-27 13:45:16 +0700

    chore: ktlint 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,99 +1,99 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	kotlin("plugin.jpa") version "1.9.22"
-	id("org.springframework.boot") version "3.4.4"
-	id("io.spring.dependency-management") version "1.1.7"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.22"
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.2"
 }
 
 group = "won"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
-	// Jsoup
-	implementation("org.jsoup:jsoup:1.17.2")
+    // Jsoup
+    implementation("org.jsoup:jsoup:1.17.2")
 
-	// Playwright
-	implementation("com.microsoft.playwright:playwright:1.44.0")
+    // Playwright
+    implementation("com.microsoft.playwright:playwright:1.44.0")
 
-	// RestAssured
-	testImplementation("io.rest-assured:rest-assured:5.5.0")
+    // RestAssured
+    testImplementation("io.rest-assured:rest-assured:5.5.0")
 
-	// H2
-	runtimeOnly("com.h2database:h2")
+    // H2
+    runtimeOnly("com.h2database:h2")
 
-	// JPA
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-	// MySQL
-	runtimeOnly("com.mysql:mysql-connector-j")
+    // MySQL
+    runtimeOnly("com.mysql:mysql-connector-j")
 
-	// logging
-	implementation ("io.github.oshai:kotlin-logging-jvm:7.0.3")
+    // logging
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
 }
 
 kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
-
 tasks.named<ProcessResources>("processResources") {
-	dependsOn("initSetting")
+    dependsOn("initSetting")
 }
 
 tasks.register("initSetting") {
-	group = "custom tasks"
-	description = "Execute both copyHooks and copySecret tasks."
+    group = "custom tasks"
+    description = "Execute both copyHooks and copySecret tasks."
 
-	dependsOn("copyHooks", "copySecret")
+    dependsOn("copyHooks", "copySecret")
 }
 
 tasks.register<Copy>("copySecret") {
-	from("./techlog-env/back")
-	include("env-common.yml")
-	into("src/main/resources")
+    from("./techlog-env/back")
+    include("env-common.yml")
+    into("src/main/resources")
 }
 
 tasks.register("copyHooks") {
-	group = "git hooks"
-	description = "Copy pre-commit and pre-push git hooks from .githooks to .git/hooks folder."
+    group = "git hooks"
+    description = "Copy pre-commit and pre-push git hooks from .githooks to .git/hooks folder."
 
-	doLast {
-		// pre-commit hook 복사
-		copy {
-			from("$rootDir/.githooks/pre-commit")
-			into("$rootDir/.git/hooks")
-		}
-		// pre-push hook 복사
-		copy {
-			from("$rootDir/.githooks/pre-push")
-			into("$rootDir/.git/hooks")
-		}
-		// pre-push hook에 실행 권한 부여
-		file("$rootDir/.git/hooks/pre-push").setExecutable(true)
-		println("Git pre-commit 및 pre-push hook이 성공적으로 등록되었습니다.")
-	}
+    doLast {
+        // pre-commit hook 복사
+        copy {
+            from("$rootDir/.githooks/pre-commit")
+            into("$rootDir/.git/hooks")
+        }
+        // pre-push hook 복사
+        copy {
+            from("$rootDir/.githooks/pre-push")
+            into("$rootDir/.git/hooks")
+        }
+        // pre-push hook에 실행 권한 부여
+        file("$rootDir/.git/hooks/pre-push").setExecutable(true)
+        println("Git pre-commit 및 pre-push hook이 성공적으로 등록되었습니다.")
+    }
 }
