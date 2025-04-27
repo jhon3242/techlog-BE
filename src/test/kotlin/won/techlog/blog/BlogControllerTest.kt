00277779414f6a19372addccf1dd98c0ff2f0415
jhon3242 <rldotnr2@gmail.com>
2025-04-27 12:58:19 +0700

    test: 블로그 파싱 테스트 주석 처리

--- src/test/kotlin/won/techlog/blog/BlogControllerTest.kt
+++ src/test/kotlin/won/techlog/blog/BlogControllerTest.kt
@@ -12,9 +12,8 @@ import won.techlog.support.BaseControllerTest
 
 class BlogControllerTest: BaseControllerTest() {
 
-    @ParameterizedTest
+//    @ParameterizedTest
 //    @ValueSource(strings = ["https://techblog.woowahan.com/?paged=1", "https://d2.naver.com/helloworld?page=0"])
-    @ValueSource(strings = ["https://d2.naver.com/helloworld?page=0"])
     fun `블로그 글을 찾는다`(url: String) {
         // given
         val request = BlogRequest(url)
@@ -28,9 +27,8 @@ class BlogControllerTest: BaseControllerTest() {
         println(result)
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = ["https://techblog.woowahan.com/21604/", "https://d2.naver.com/helloworld/1168674"])
-//    @ValueSource(strings = ["https://d2.naver.com/helloworld/1168674"])
+//    @ParameterizedTest
+//    @ValueSource(strings = ["https://techblog.woowahan.com/21604/", "https://d2.naver.com/helloworld/1168674"])
     fun `블로그 글을 파싱한다`(url: String) {
         // given
         val request = BlogRequest(url)
