7a0216bd3bcffe32338b7420be0c4e249700ba9f
jhon3242 <rldotnr2@gmail.com>
2025-05-01 17:25:46 +0700

    feat: healthCheck 컨트롤러 추가

---
+++ src/main/kotlin/won/techlog/common/web/HealthCheckController.kt
@@ -0,0 +1,13 @@
+package won.techlog.common.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthCheckController {
+    @GetMapping("/health")
+    fun healthCheck(): String {
+        return "I'm OK"
+    }
+}
+
