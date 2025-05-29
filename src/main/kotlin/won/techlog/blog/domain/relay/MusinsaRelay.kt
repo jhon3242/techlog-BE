02684b35c8cffa2103cbf1abf8fba32a4f05fe16
jhon3242 <rldotnr2@gmail.com>
2025-05-29 18:11:45 +0900

    feat: 무신사 기술 블로그 relay 추가

---
+++ src/main/kotlin/won/techlog/blog/domain/relay/MusinsaRelay.kt
@@ -0,0 +1,30 @@
+package won.techlog.blog.domain.relay
+
+import org.springframework.stereotype.Component
+import won.techlog.blog.domain.BlogMetaData
+import won.techlog.blog.domain.BlogType
+import won.techlog.blog.domain.crawler.MediumBlogCrawler
+
+@Component
+class MusinsaRelay : Relayable {
+    companion object {
+        private const val SLUG = "musinsa-tech"
+        private val yearList = listOf(2021, 2022, 2023, 2024, 2025)
+    }
+
+    override fun getBlogs(): List<BlogMetaData> {
+        val result = mutableListOf<BlogMetaData>()
+        yearList.forEach { year ->
+            result.addAll(MediumBlogCrawler.fetchBlog(SLUG, year))
+        }
+        return result
+    }
+
+    override fun getBlog(url: String): BlogMetaData {
+        TODO("Not yet implemented")
+    }
+
+    override fun isSupportType(blogType: BlogType): Boolean {
+        return blogType == BlogType.MUSINSA
+    }
+}
