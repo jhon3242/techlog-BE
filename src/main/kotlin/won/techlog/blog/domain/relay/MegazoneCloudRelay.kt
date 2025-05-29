cae79c3866e1c31735fa0c924c20057b7715000f
jhon3242 <rldotnr2@gmail.com>
2025-05-29 18:13:31 +0900

    feat: 메가존 클라우드 기술 블로그 relay 추가

---
+++ src/main/kotlin/won/techlog/blog/domain/relay/MegazoneCloudRelay.kt
@@ -0,0 +1,30 @@
+package won.techlog.blog.domain.relay
+
+import org.springframework.stereotype.Component
+import won.techlog.blog.domain.BlogMetaData
+import won.techlog.blog.domain.BlogType
+import won.techlog.blog.domain.crawler.MediumBlogCrawler
+
+@Component
+class MegazoneCloudRelay : Relayable {
+    companion object {
+        private const val SLUG = "cloudvillains"
+        private val yearList = listOf(2021, 2022, 2023, 2024)
+    }
+
+    override fun getBlogs(): List<BlogMetaData> {
+        val result = mutableListOf<BlogMetaData>()
+        yearList.forEach { year ->
+            result.addAll(MediumBlogCrawler.fetchBlog(SLUG, year))
+        }
+        return result
+    }
+
+    override fun getBlog(url: String): BlogMetaData {
+        TODO("Not yet implemented")
+    }
+
+    override fun isSupportType(blogType: BlogType): Boolean {
+        return blogType == BlogType.MEGAZONE_CLOUD
+    }
+}
